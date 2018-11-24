ff775952f0e793045b2a9aa9f7305ca4a8e456d6
Naveed <naveedfarouq@hotmail.co.uk>
2018-11-24 17:50:04 +0000

    fixing running issues

---
+++ src/main/java/com/in28Minutes/database/databasedemo/jpa/PersonJpaRepository.java
@@ -0,0 +1,12 @@
+package com.in28Minutes.database.databasedemo.jpa;
+
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+
+
+}
